1a57ca67e80b5af4c7652e757fd9cd56d1948983
Jacob Morris <jacobthomasmorris@gmail.com>
2018-08-20 10:26:29 -0600

    spacing corrections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,7 @@ var audio = new Audio(
 $("button").on("click", next);
 var time = $("#time");
 var userAnswer;
+
 function next() {
     displayQuestion(count);
 }
@@ -24,74 +25,73 @@ function displayQuestion(j) {
             data["results"][j]["incorrect_answers"][1],
             data["results"][j]["incorrect_answers"][2]
         ];
-        function populate() {
-            $("#correct").html(correctAnswers);
-            $("#wrong").html(wrongAnswers);
-            $("#question").html(data["results"][j]["question"]);
-            var i = Math.floor(Math.random() * 4);
 
-            $("#answer1").html(answers[i]);
-            answers.splice(i, 1);
-            i = Math.floor(Math.random() * 3);
-            $("#answer2").html(answers[i]);
-            answers.splice(i, 1);
-            i = Math.floor(Math.random() * 2);
-            $("#answer3").html(answers[i]);
-            answers.splice(i, 1);
+        $("#correct").html(correctAnswers);
+        $("#wrong").html(wrongAnswers);
+        $("#question").html(data["results"][j]["question"]);
+        var i = Math.floor(Math.random() * 4);
 
-            $("#answer4").text(answers[0]);
-            $(".answer").off("click");
-            $(".answer").on("click", function() {
-                console.log("clicked");
-                checkAnswer(
-                    this.innerHTML,
-                    data["results"][j]["correct_answer"]
-                );
+        $("#answer1").html(answers[i]);
+        answers.splice(i, 1);
+        i = Math.floor(Math.random() * 3);
+        $("#answer2").html(answers[i]);
+        answers.splice(i, 1);
+        i = Math.floor(Math.random() * 2);
+        $("#answer3").html(answers[i]);
+        answers.splice(i, 1);
+
+        $("#answer4").text(answers[0]);
+        $(".answer").off("click");
+
+        $(".answer").on("click", function() {
+            checkAnswer(this.innerHTML, data["results"][j]["correct_answer"]);
+        });
+        $("#category").html(data["results"][j]["category"]);
+        $("#difficulty").html(data["results"][j]["difficulty"]);
+        $(".answer").show();
+        $("#scoreboard").show();
+        $("button").hide();
+        $("#info").hide();
+        if (count >= 8) {
+            $("button").show();
+            $(".answer").hide();
+            $("#question").html("Quiz complete good Job <br>");
+            $("button").html("Play Again");
+            clearInterval(interval);
+            $("button").off("click");
+            $("button").on("click", function() {
+                $("button").off("click");
+                $("button").on("click", next);
+                correctAnswers = 0;
+                wrongAnswers = 0;
+                count = 0;
+                displayQuestion(count);
             });
-            $("#category").html(data["results"][j]["category"]);
-            $("#difficulty").html(data["results"][j]["difficulty"]);
-            $(".answer").show();
-            $("#scoreboard").show();
-            $("button").hide();
-            $("#info").hide();
-            if (count >= 8) {
-                $("button").show();
-                $(".answer").hide();
-                $("#question").html("Quiz complete good Job");
-                $("button").html("Play Again");
-                clearInterval(interval);
-                $("button").on("click", function() {
-                    location.reload();
-                });
-            }
-            time.html(30);
         }
-        setTimeout(populate, 1000);
+        time.html(30);
 
         function checkAnswer(userGuess, correctAnswer) {
             count++;
-            console.log("made it to function");
             clearInterval(interval);
             $(".answer").hide();
             $("#scoreboard").hide();
 
             if (userGuess == correctAnswer) {
-                console.log("right");
                 correctAnswers++;
                 $("#correct").html(correctAnswers);
-                $("#question").html("RIGHT!");
-                setTimeout(next, 500);
+                $("#question").html("RIGHT!<br> <Br> <br> <br> <br>");
+                setTimeout(next, 1000);
             } else {
-                console.log("wrong");
                 wrongAnswers++;
                 $("#wrong").html(wrongAnswers);
                 $("#question").html("WRONG!");
                 $("#info").show();
                 $("#info").html(
                     "The correct answer was: " +
-                        data["results"][j]["correct_answer"]
+                        data["results"][j]["correct_answer"] +
+                        "<br> <Br> <br> <br>    "
                 );
-                setTimeout(next, 100);
+                setTimeout(next, 1500);
             }
         }
 
@@ -108,7 +108,6 @@ function timer() {
         $("button").show();
         $("#question").html("Times Up");
         time.html("TIMES UP!");
-        console.log("wrong");
         wrongAnswers++;
         $("#wrong").html(wrongAnswers);
 
